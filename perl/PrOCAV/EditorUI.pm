c0b568072135046e168cda4347e9ddd8ed0a3510
Richard Lewis <richard.lewis@gold.ac.uk>
2011-12-11 23:42:49 +0000

    Added required_parameters option to login function

--- perl/PrOCAV/EditorUI.pm
+++ perl/PrOCAV/EditorUI.pm
@@ -24,6 +24,7 @@ my $TEMPLATES_DIR = "../../web/editor";
 
 our %login = (
     uri_pattern => qr/^\/login/,
+    required_parameters => [qw(login_name password)],
     handle => sub {
 	my ($r, $req) = @_;
 
