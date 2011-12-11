3ae6af4605f8ecb43c32831eb6b34b3ecc17f157
Richard Lewis <richard.lewis@gold.ac.uk>
2011-12-11 12:59:26 +0000

    Added PrOCAV::EditorUI module stub

---
+++ perl/PrOCAV/EditorUI.pm
@@ -0,0 +1,12 @@
+#
+# PrOCAV
+#
+# This module provides URL handlers for the editors' user interface
+#
+# Author: Richard Lewis
+# Email: richard.lewis@gold.ac.uk
+
+use strict;
+use HTML::Template;
+
+1;
