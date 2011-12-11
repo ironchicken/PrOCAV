ff80402aabf9385ddecf7889f288b164d65d6f54
Richard Lewis <richard.lewis@gold.ac.uk>
2011-12-11 23:13:56 +0000

    Started implementing the HTTP API handling

--- perl/PrOCAV/API.pm
+++ perl/PrOCAV/API.pm
@@ -1,11 +1,79 @@
 #
 # PrOCAV
 #
-# This modules exports public API functions which are mapped to URLs.
+# This module exports public API functions which are mapped to
+# URLs. FIXME No, it does not.
 #
 # Author: Richard Lewis
 # Email: richard.lewis@gold.ac.uk
 
 use strict;
+use Apache2::Request;
+#use Apache2::RequestRec ();
+#use Apache2::RequestIO ();
+use Apache2::Const -compile => qw(:common :log);
+use Apache2::Cookie;
+use Apache2::Log;
+use APR::Const -compile => qw(:error SUCCESS);
+#use Apache2::Ajax;
+use JSON;
+use PrOCAV::Database qw(session);
+use PrOCAV::EditorUI qw(%login %new_session %generate_template %submit_tables);
+
+package PrOCAV::API;
+
+sub authorised {
+    my ($r, $handler) = @_;
+
+    if (not exists $handler->{authorisation}) { return 1; }
+
+    my %in_cookies = Apache2::Cookie->fetch($r);
+
+    my $sid_name;
+    $sid_name = "provac_editor_sid"   if ($handler->{authorisation} eq "editor");
+    $sid_name = "provac_consumer_sid" if ($handler->{authorisation} eq "consumer");
+
+    if (not defined $sid_name) {
+	die("No matching authorisation scheme: " . $handler->{authorisation} . "\n");
+    }
+
+    my $session_id = $in_cookies{$sid_name} && $in_cookies{$sid_name}->value;
+
+    if ($handler->{authorisation} eq "editor") {
+	my $login_name = $in_cookies{"login_name"} && $in_cookies{"login_name"}->value;
+	return Database::session("editor", $login_name, $session_id);
+    }
+
+    if ($handler->{authorisation} eq "consumer") {
+	return 0;
+    }
+}
+
+sub handler {
+    my $r = shift;
+    my $req = Apache2::Request->new($r);
+
+    my @DISPATCH_TABLE = (
+	\%PrOCAV::EditorUI::login,
+	\%PrOCAV::EditorUI::new_session
+	);
+
+    my $s = $r->server;
+
+    foreach my $h (@DISPATCH_TABLE) {
+
+	$s->log_error(sprintf("Test %s against %s", $r->uri, $h->{uri_pattern}));
+
+	if ($r->uri =~ $h->{uri_pattern}) {
+	    $s->log_error(sprintf("%s matches %s", $r->uri, $h->{uri_pattern}));
+
+	    return Apache2::Const::FORBIDDEN if (not authorised $r, $h);
+
+	    return &{$h->{handle}}($r, $req);
+	}
+    }
+
+    return Apache2::Const::NOT_FOUND;
+}
 
 1;
