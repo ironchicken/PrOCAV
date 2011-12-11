eccc8db0a47c5f1e38f99f349fc0c9cb04f0dafb
Richard Lewis <richard.lewis@gold.ac.uk>
2011-12-11 12:59:36 +0000

    Added PrOCAV::PublicUI module stub

---
+++ perl/PrOCAV/PublicUI.pm
@@ -0,0 +1,12 @@
+#
+# PrOCAV
+#
+# This module provides URL handlers for the public Web user interface
+#
+# Author: Richard Lewis
+# Email: richard.lewis@gold.ac.uk
+
+use strict;
+use HTML::Template;
+
+1;
