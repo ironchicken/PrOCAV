782e72efb6b8c74c6546dfeab4f7f18a0f01d455
Richard Lewis <richard@rjlewis.me.uk>
2012-04-16 11:46:22 +0100

    Added ComposerCat::Search module stub

---
+++ perl/ComposerCat/Search.pm
@@ -0,0 +1,21 @@
+#
+# ComposerCat
+#
+# This module provides interfaces to various search facilities for
+# ComposerCat databases, included a fulltext indexer and an advanced
+# search.
+#
+# Author: Richard Lewis
+# Email: richard.lewis@gold.ac.uk
+
+package ComposerCat::Search;
+
+use strict;
+
+BEGIN {
+    use Exporter;
+    our @ISA = qw(Exporter);
+    our @EXPORT_OK = qw();
+}
+
+1;
