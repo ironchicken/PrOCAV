22879a5a3077521c5967f16b131e0802e9208927
Richard Lewis <richard@rjlewis.me.uk>
2012-06-14 17:05:19 +0100

    Altered manuscripts._pages query to include media_items

--- perl/ComposerCat/Schema.pm
+++ perl/ComposerCat/Schema.pm
@@ -3015,12 +3015,17 @@ sub schema_prepare_statments {
     $schema{manuscripts}->{_pages} =
 	$dbh->prepare(q|SELECT document_pages.ID AS document_page_id, page_number, page_side, page_label, document_pages.notes AS page_notes,
     aggregations.label, aggregations.label_num, aggregations.title, aggregations.level, aggregations.extent_stmt, aggregations.description,
-    parent_aggr.ID AS parent_aggr_id, parent_aggr.label AS parent_aggr_label, parent_aggr.level AS parent_aggr_level
+    parent_aggr.ID AS parent_aggr_id, parent_aggr.label AS parent_aggr_label, parent_aggr.level AS parent_aggr_level,
+    media_items.ID AS media_id, media_items.mime_type, media_items.path, media_items.content_type, media_items.extent, media_items.resolution,
+    media_items.date_made, media_items.date_acquired, media_items.copyright, media_items.public, representation_of.relation
     FROM document_pages
     LEFT JOIN in_archive ON in_archive.page_id = document_pages.ID
     LEFT JOIN aggregations ON aggregations.ID = in_archive.aggregation_id
     LEFT JOIN aggregations AS parent_aggr ON aggregations.parent = parent_aggr.ID
+    LEFT JOIN representation_of ON representation_of.related_id = document_pages.ID
+    LEFT JOIN media_items ON media_items.ID = representation_of.media_id
     WHERE document_pages.document_id = ?
+      AND (representation_of.related_table = "document_pages" OR representation_of.related_table IS NULL)
     ORDER BY parent_aggr.label_num, page_number, page_side, page_label|);
 
     # manuscripts.title_source
