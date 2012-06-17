43e6525b948af4a2edc18eb268ce30a86e597651
Richard Lewis <richard.lewis@gold.ac.uk>
2012-06-18 00:08:39 +0100

    Added ComposerCat::Digitisations module stub

---
+++ perl/ComposerCat/Digitisations.pm
@@ -0,0 +1,20 @@
+#
+# ComposerCat
+#
+# This module provides methods to render and search digitisations
+#
+# Author: Richard Lewis
+# Email: richard.lewis@gold.ac.uk
+
+package ComposerCat::Digitisations;
+
+use strict;
+
+BEGIN {
+    use Exporter;
+    our @ISA = qw(Exporter);
+    our @EXPORT_OK = qw();
+}
+
+
+1;
